bb03d65789f2ea1e876dd0283f96c490dccedcbb
fancy200512 <3588922001@qq.com>
2026-04-20 20:59:17 +0800

    Delete backend directory

--- backend/server/src/main/java/com/cy/mapper/ImageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cy.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cy.entity.Image;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface ImageMapper extends BaseMapper<Image> {
-}
--- backend/server/src/main/java/com/cy/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.cy.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.cy.entity.Session;
-import com.cy.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-
-public interface UserMapper extends BaseMapper<User> {
-}
